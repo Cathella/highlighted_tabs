7bca3d963b1ed4d921c097d2337edd5cdb59cb17
Cathella <ncathie214@gmail.com>
2022-02-02 14:41:28 +0300

    Add mouse move events

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+
+function Tab({ children }) {
+  const [ highlighted, setHighlighted ] = useState({ left: 0, opacity: 0 });
+
+  function moveHighlight(e) {
+    // update highlight style
+    setHighlighted({
+      left: e.nativeEvent.layerX-150,
+    })
+  }
+
+  function hideHighlight(e) {
+    setHighlighted({
+      opacity: 0,
+      left: e.nativeEvent.layerX-150,
+    })
+  }
+
+  return (
+    <div className="tab" onMouseMove={moveHighlight} onMouseOut={hideHighlight}>
+      <div className="highlight" style={highlighted} />
+      {children}
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="app">
       <div className="browser">
         <div className="tabs">
-          <div className="tab">
-            <a>Home</a>
-          </div>
-          <div className="tab">
-            <a>About</a>
-          </div>
-          <div className="tab">
-            <a>Features</a>
-          </div>
+          <Tab><a>Home</a></Tab>
+          <Tab><a>About</a></Tab>
+          <Tab><a>Features</a></Tab>
         </div>
 
         <div className="viewport">Pages go Here</div>
